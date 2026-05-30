dc9e63f1015dd449d299c81236848a16e2c173ac
pabloamicodev <pabloamicodev@users.noreply.github.com>
2026-05-29 22:54:34 -0300

    test: plain div render - no Shopify web components

--- extensions/marginlab-checkout/src/MarginLabBlock.jsx
+++ extensions/marginlab-checkout/src/MarginLabBlock.jsx
@@ -1,9 +1,5 @@
 import "@shopify/ui-extensions/preact";
 
 export function MarginLabBlock() {
-  return (
-    <s-box paddingBlock="base" paddingInline="none">
-      <s-text type="base">MarginLab A/B Test Block</s-text>
-    </s-box>
-  );
+  return <div>MARGINLAB-TEST-RENDER</div>;
 }
